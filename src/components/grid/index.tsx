7ca6d677858e6aba0929895520b6a31d0d0573e3
Евгений Минин <vnp@cappers.ca>
2020-10-25 10:19:15 +0300

    added copy grid

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC, Children, useCallback, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Dispatch, AnyAction } from 'redux'
-import { useHotkeys } from 'react-hotkeys-hook'
 
 import { INDEX } from 'typings'
 import { createGrid } from 'reducers'
@@ -17,27 +16,6 @@ const Grid: FC = () => {
   useEffect(() => {
     create()
   }, [create])
-
-  const moveDown = () => {
-    console.log('down')
-  }
-
-  const moveUp = () => {
-    console.log('up')
-  }
-
-  const moveLeft = () => {
-    console.log('left')
-  }
-
-  const moveRight = () => {
-    console.log('right')
-  }
-
-  useHotkeys('down', moveDown)
-  useHotkeys('up', moveUp)
-  useHotkeys('left', moveLeft)
-  useHotkeys('right', moveRight)
 
   return (
     <Container data-cy="grid-container">
